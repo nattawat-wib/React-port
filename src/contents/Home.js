665ca8f88ab681955e3c34ee3dd66d1cf83a7496
nattawat.wib <nattawat.wib@gmail.com>
2020-11-13 21:51:46 +0700

    edit html

--- src/contents/Home.js
+++ src/contents/Home.js
@@ -5,11 +5,11 @@ import ReactTypingEffect from 'react-typing-effect';
 
 class Home extends Component {
     render() {
-        return(
+        return (
             <div className="condiv home">
-                <img src='https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=3000&h=3000&q=80' alt="profile" className="profilepic"/>
-                <ReactTypingEffect text={["i'm Nut ella", "i'm Wev developer"]} speed={80} eraseDelay={2000} eraseSpeed={80} className='typingeffect' />
-                <Social/>
+                <img src='https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=3000&h=3000&q=80' alt="profile" className="profilepic" />
+                <ReactTypingEffect text={["Hi, Welcome.", "i'm Nut ella", "i'm Wev developer"]} speed={80} eraseDelay={1000} eraseSpeed={80} typingDelay={500} className='typingeffect' />
+                <Social />
             </div>
         );
     }
