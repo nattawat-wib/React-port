ead7e14b19fdf41293ab5d6c174cfb5f36b85067
nattawat.wib <nattawat.wib@gmail.com>
2020-11-13 13:19:32 +0700

    test deploy

--- src/App.js
+++ src/App.js
@@ -7,8 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
     <p>
-      <a style={{color: 'white', textDecoration: 'none'}} href="https://www.google.com" target="__blank"> First Link </a> <br></br><br></br>
-      <a style={{color: 'white', textDecoration: 'none'}} href="https://nattawat-port.web.app/Dark%20mode/dark.html" target="__blank"> Second Link </a>
+
     </p>
         <a
           className="App-link"
